2123a119673c1cfe7ce075a53ff8301aaf5affd0
root <root@localhost.localdomain>
2016-02-03 09:18:42 -0500

    delete

--- appbuffer.c
+++ appbuffer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
 
 #define DEVICE "/dev/testdevice"
 
@@ -14,18 +15,21 @@ int main(void){
 			printf("file %s n'existe pas ou vous n'avez pas les permissions pour faire ca\n", DEVICE);
 			exit(-1);
 	}
-	printf ("r = Affiche le contenu du device\nw = Ecrire du contenu dan le device\nVotre commande : ");
+	printf ("r = Affiche le contenu du device\nw = Ecrire du contenu dan le device\nd = Vide le device\nVotre commande : ");
 	scanf("%c", &ch);
 
 	switch (ch) {
 			case 'w':
-				printf("Rentrer votre contenu :");
+					printf("Rentrer votre contenu :");
 					scanf(" %[^\n]", write_buf);
 					write(fd, write_buf, sizeof(write_buf));
 					break;
 			case 'r':
 					read(fd, read_buf, sizeof(read_buf));
 					printf("device: %s\n", read_buf);
+					break;
+			case 'd':
+					write(fd, NULL, sizeof(NULL));
 					break;
 			default:
 					printf("Commande non reconnues\n");
